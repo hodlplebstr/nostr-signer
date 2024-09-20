3a0a92823f33512082182fbd8db3d60481603b87
HodlPleb <hodl@plebstr.xyz>
2024-09-20 10:41:54 -0500

    cleanup

--- app.mjs
+++ app.mjs
@@ -1,23 +1,18 @@
 import 'dotenv/config'
-import { bytesToHex, hexToBytes } from '@noble/hashes/utils'
-import { generateSecretKey, getPublicKey, finalizeEvent, verifyEvent } from 'nostr-tools/pure'
-import { bech32, bech32m } from 'bech32';
+
+import { bytesToHex } from '@noble/hashes/utils'
+import { finalizeEvent, verifyEvent, getPublicKey } from 'nostr-tools/pure'
+import { bech32 } from 'bech32';
 
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 
 
-function bech32Decoder(currPrefix, data) {
-  const { prefix, words } = bech32.decode(data);
-  if (prefix !== currPrefix) {
-      throw Error('Invalid address format');
-  }
-  return Buffer.from(bech32.fromWords(words));
-}
 
 const rl = readline.createInterface({ input, output });
 let nsec = await rl.question('Enter your nsec -or- press [enter] for .env: ');
 const eventContent = await rl.question('Enter your Note Text: ');
+rl.close();
 
 nsec = (!nsec) ? process.env.NSEC : nsec;
 nsec = (nsec.slice(0,4) !== "nsec") ? "nsec" + nsec : nsec;
@@ -59,4 +54,14 @@ if (!verifyEvent(event)) {
 
   console.log(eventJson);
   process.exit(1);
+}
+
+// bech32Decoder outputs an array of octets (Uint8array)
+// from https://github.com/michaelleighstewart/nostr-client/blob/3f5c6d293c83b94860e76d32599143554f335444/src/utils/helperFunctions.ts#L50
+function bech32Decoder(currPrefix, data) {
+  const { prefix, words } = bech32.decode(data);
+  if (prefix !== currPrefix) {
+      throw Error('Invalid nsec format');
+  }
+  return Buffer.from(bech32.fromWords(words));
 }
